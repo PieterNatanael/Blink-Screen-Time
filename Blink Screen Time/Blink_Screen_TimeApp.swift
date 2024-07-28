6247d29fc73a4fc204a0e977b524db2fae662c47
PieterNatanael <pieternatanael@icloud.com>
2024-07-28 18:23:25 +0800

    Initial Commit

---
+++ Blink Screen Time/Blink_Screen_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blink_Screen_TimeApp.swift
+//  Blink Screen Time
+//
+//  Created by Pieter Yoshua Natanael on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Blink_Screen_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
